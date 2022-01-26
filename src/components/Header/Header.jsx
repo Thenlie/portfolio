bab85492355c8c1cdf832c46bef989f64eb553b8
Leithen <leithen113@gmail.com>
2022-01-26 15:15:33 -0700

    Added hero photo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ import Navigation from '../Navigation';
 function Header(props) {
     const { currentCategory, setCurrentCategory } = props;
     return (
-        <header className='d-flex justify-content-between p-2'>
+        <header className='d-flex justify-content-between p-2' style={{backgroundImage: `url('/assets/tie-dye.jpg')`, backgroundSize: 'cover'}}>
             <h1>Leithen</h1>
             <Navigation currentCategory={currentCategory} setCurrentCategory={setCurrentCategory} />
         </header>
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,7 +1,7 @@
 function Navigation(props) {
     const {currentCategory, setCurrentCategory} = props;
     return (
-        <ul className='d-flex list-group-horizontal'>
+        <ul className='d-flex list-group-horizontal mb-0'>
             <li className={`m-1 list-group-item ${currentCategory === 'about' && 'text-warning'}`} onClick={() => setCurrentCategory('about')}>About Me</li>
             <li className={`m-1 list-group-item ${currentCategory === 'portfolio' && 'text-warning'}`} onClick={() => setCurrentCategory('portfolio')}>Portfolio</li>
             <li className={`m-1 list-group-item ${currentCategory === 'contact' && 'text-warning'}`} onClick={() => setCurrentCategory('contact')}>Contact</li>
