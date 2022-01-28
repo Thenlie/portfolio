c4e09716f9e6c0d67516a444ddb8bd359c3ad841
Leithen <leithen113@gmail.com>
2022-01-28 14:40:35 -0700

    Small styling changes

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -2,7 +2,7 @@ function About() {
     return (
         <section>
             <h1 className='text-center'>About</h1>
-            <p>
+            <p className='m-4'>
                 Full Stack Web Developer with a background in manufacturing and customer service. Recognized as a fast learner, excellent problem solver and effective collaborator. Recent graduate of the University of Utah’s full stack coding bootcamp. 
             </p>
         </section>
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,11 +1,11 @@
 function Navigation(props) {
     const {currentCategory, setCurrentCategory} = props;
     return (
-        <ul className='d-flex list-group-horizontal mb-0'>
-            <li className={`m-1 list-group-item ${currentCategory === 'about' && 'text-warning'}`} onClick={() => setCurrentCategory('about')}>About Me</li>
-            <li className={`m-1 list-group-item ${currentCategory === 'portfolio' && 'text-warning'}`} onClick={() => setCurrentCategory('portfolio')}>Portfolio</li>
-            <li className={`m-1 list-group-item ${currentCategory === 'contact' && 'text-warning'}`} onClick={() => setCurrentCategory('contact')}>Contact</li>
-            <li className={`m-1 list-group-item ${currentCategory === 'resume' && 'text-warning'}`} onClick={() => setCurrentCategory('resume')}>Resume</li>
+        <ul className='d-flex list-group-horizontal mb-0 h-50 mt-4'>
+            <li className={`m-1 list-group-item list-group-item-dark ${currentCategory === 'about' && 'text-primary'}`} onClick={() => setCurrentCategory('about')}>About Me</li>
+            <li className={`m-1 list-group-item list-group-item-dark ${currentCategory === 'portfolio' && 'text-primary'}`} onClick={() => setCurrentCategory('portfolio')}>Portfolio</li>
+            <li className={`m-1 list-group-item list-group-item-dark ${currentCategory === 'contact' && 'text-primary'}`} onClick={() => setCurrentCategory('contact')}>Contact</li>
+            <li className={`m-1 list-group-item list-group-item-dark ${currentCategory === 'resume' && 'text-primary'}`} onClick={() => setCurrentCategory('resume')}>Resume</li>
         </ul>
     );
 }
